e6c799e7a5cf5bfebe41dff0b8f223d5c1d46450
Tyz46 <thomas.bruz@pucpr.edu.br>
2025-05-06 09:04:56 -0300

    3 Exercicios de Lista
    Soma, Inverso e Média.

---
+++ Média_Lista.py
@@ -0,0 +1,6 @@
+notas = [2, 10, 5, 7]
+print('suas notas:', notas)
+
+soma = sum(notas)
+media = soma / 4
+print('sua média:', media)
---
+++ RealInverso_Lista.py
@@ -0,0 +1,3 @@
+lista = [-1, -2, 0, 2, 3, 6, 10, 20, 2.1, 1]
+lista_invertida = lista[::-1]
+print(lista_invertida)
